331b1515e445c759f2b432ba59fd8585d57e4194
Chris Battis <chris.battis@gmail.com>
2025-05-23 16:35:00 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',  // Explicitly set root to project directory
-  build: {
-    outDir: 'dist'  // Ensure build output directory is correct
-  }
+  base: './'
 })
